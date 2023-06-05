0bce5e86e60f5f5c74d3b48b82cb23bfe7edc3a2
Wojciech Bogobowicz <wojciech.bogobowicz@gmail.com>
2023-06-05 18:07:33 +0200

    env: model paths added

--- config/paths.py
+++ config/paths.py
@@ -1,8 +1,10 @@
 import pathlib
 
 CONFIG = pathlib.Path(__file__).parent
-PROJECT = CONFIG.parent
-DATA_DIR = PROJECT / "data"
+ROOT = CONFIG.parent
+DATA_DIR = ROOT / "data"
+SAVED_MODELS = DATA_DIR / "models"
+MODELS_CONFIG = CONFIG / "model"
 SINGLE_PARTICLE_FILE = DATA_DIR / "01_raw" / "S_random_1.xyz"
 PARTICLES_FILE = DATA_DIR / "01_raw" / "S_random_8000.xyz"
 TRANSPORT_FILE = DATA_DIR / "01_raw" / "S_random_8000.trans"
